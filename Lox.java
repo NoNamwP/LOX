54e93e1921ee26d079fb7c88e580784beca477d9
NoNamwP <natalia.neczek.p@gmail.com>
2025-10-30 14:44:04 +0100

    Implement parser and interpreter integration

--- Lox.java
+++ Lox.java
@@ -10,7 +10,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Lox {
+
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         
         if (args.length > 1) {
@@ -28,6 +34,8 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
 
          if (hadError) System.exit(65);
+
+         if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -47,10 +55,13 @@ public class Lox {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+     Parser parser = new Parser(tokens);
+     List<Stmt> statements = parser.parse();
+
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+     interpreter.interpret(statements);
   }
 static void error(int line, String message) {
     report(line, "", message);
@@ -60,6 +71,20 @@ static void error(int line, String message) {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
 }
